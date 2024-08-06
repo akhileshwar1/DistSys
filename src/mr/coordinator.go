d96ee4b8cd8dbad4bf73a3f5d616bf18d649170c
Akhil Kandi <akhilprm999@gmail.com>
2024-08-06 23:30:34 +0530

    Change coordinator state to include three stacks of tasks instead of a slice of tasks.
    Helps get the status of the task from the worker, will be needed to
    detect crashed workers.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -10,65 +10,117 @@ import "errors"
 import "io/ioutil"
 import "sync"
 
+type Stack []task
+
+func (s *Stack) push(value task) {
+  *s = append(*s, value)
+}
+
+func (s *Stack) pop() (task, bool) {
+  if len(*s) == 0 {
+    return task{}, false // stack is empty.
+  }
+
+  index := len(*s) - 1
+  value := (*s)[index]
+  *s = (*s)[:index] // resize the stack, effectively removes the last element.
+  return value, true
+}
+
+// returns the removed task.
+func (s *Stack) remove(filename string) (task, bool) {
+  for i, task := range *s {
+    if task.filename == filename {
+      *s = append((*s)[:i], (*s)[i+1:]...)
+      return task, true
+    }
+  }
+
+  return task{}, false
+}
+
+func (s *Stack) peek() (task, bool) {
+  if len(*s) == 0 {
+    return task{}, false // stack is empty.
+  }
+
+  index := len(*s) - 1
+  value := (*s)[index]
+  return value, true
+}
+
 type task struct {
   filename string
-  status int  // 0: not started, 1: started, 2: done.
   operation string // "map" or "reduce"
 }
 
 type Coordinator struct {
   mu sync.Mutex
-  tasks []task
-  current int  // stores the current index of the tasks list.
+  startedTasks Stack 
+  notStartedTasks Stack 
+  doneTasks Stack 
   nReduce int  // no of nReduce files to distribute the keys to.
 }
 
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
-	return nil
+  reply.Y = args.X + 1
+  return nil
 }
 
 func (c *Coordinator) GetTask(args *struct{}, reply *TaskReply) error {
-    c.mu.Lock()
-    defer c.mu.Unlock()
-    if c.current >= len(c.tasks) {
-        return errors.New("No tasks available")
-    }
+  c.mu.Lock()
+  defer c.mu.Unlock()
+  task, bool := c.notStartedTasks.pop()
+  if !bool {
+    return errors.New("No tasks available")
+  }
 
-    filename := c.tasks[c.current].filename
-    file, err := os.Open(filename)
-    if err != nil {
-        return errors.New("cannot open " + filename)
-    }
-    defer file.Close()  // Ensure the file is closed even if there's an error
+  filename := task.filename
+  file, err := os.Open(filename)
+  if err != nil {
+    return errors.New("cannot open " + filename)
+  }
+  defer file.Close()  // Ensure the file is closed even if there's an error
 
-    content, err := ioutil.ReadAll(file)
-    if err != nil {
-        return errors.New("cannot read " + filename)
-    }
+  content, err := ioutil.ReadAll(file)
+  if err != nil {
+    return errors.New("cannot read " + filename)
+  }
 
-    reply.Filename = filename
-    reply.Content = string(content)
-    reply.Nreduce = c.nReduce
-    reply.Operation = c.tasks[c.current].operation
-    c.current++  // Move to the next task
-    return nil
+  reply.Filename = filename
+  reply.Content = string(content)
+  reply.Nreduce = c.nReduce
+  reply.Operation = task.operation
+
+  c.startedTasks.push(task) // push to started status.
+  return nil
+}
+
+func (c *Coordinator) DoneTask(filename string, reply *TaskReply) error {
+  task, bool := c.startedTasks.remove(filename)
+  if !bool {
+    return errors.New("No such task")
+  }
+
+  // push it to the done stack.
+  c.doneTasks.push(task)
+  return nil
 }
 
 //
 // start a thread that listens for RPCs from worker.go
 //
 func (c *Coordinator) server() {
-	rpc.Register(c)
-	rpc.HandleHTTP()
-	//l, e := net.Listen("tcp", ":1234")
-	sockname := coordinatorSock()
-	os.Remove(sockname)
-	l, e := net.Listen("unix", sockname)
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	go http.Serve(l, nil)
+  rpc.Register(c)
+  rpc.HandleHTTP()
+  //l, e := net.Listen("tcp", ":1234")
+  sockname := coordinatorSock()
+  os.Remove(sockname)
+  l, e := net.Listen("unix", sockname)
+  if e != nil {
+    log.Fatal("listen error:", e)
+  }
+  go http.Serve(l, nil)
 }
 
 //
@@ -76,11 +128,13 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-  if c.current >= len(c.tasks) {
+  _, nbool := c.notStartedTasks.peek()
+  _, sbool := c.startedTasks.peek()
+  if (!nbool && !sbool) {
     return true
   }
 
-	return false
+  return false
 }
 
 //
@@ -89,28 +143,23 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
-  
-  // add map tasks
-  for _, name := range files {
-    t := task{name, 0, "map"}
-    c.tasks = append(c.tasks, t)
-  }
+  c := Coordinator{}
 
   // add reduce tasks
   i := 0
   for i < nReduce {
-    t := task{fmt.Sprintf("mr-%d", i), 0, "reduce"}
-    c.tasks = append(c.tasks, t)
+    t := task{fmt.Sprintf("mr-%d", i), "reduce"}
+    c.notStartedTasks.push(t)
     i++
   }
 
-  c.current = 0
+  // add map tasks
+  for _, name := range files {
+    t := task{name, "map"}
+    c.notStartedTasks.push(t)
+  }
+
   c.nReduce = nReduce
-
-  fmt.Println(c)
-
-	c.server()
-	return &c
+  c.server()
+  return &c
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -21,8 +21,8 @@ func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 // Map functions return a slice of KeyValue.
 type KeyValue struct {
-	Key   string
-	Value string
+  Key   string
+  Value string
 }
 
 //
@@ -30,9 +30,9 @@ type KeyValue struct {
 // task number for each KeyValue emitted by Map.
 //
 func ihash(key string) int {
-	h := fnv.New32a()
-	h.Write([]byte(key))
-	return int(h.Sum32() & 0x7fffffff)
+  h := fnv.New32a()
+  h.Write([]byte(key))
+  return int(h.Sum32() & 0x7fffffff)
 }
 
 
@@ -41,7 +41,7 @@ func ihash(key string) int {
 //
 
 func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+  reducef func(string, []string) string) {
   x := 5
   ticker := time.NewTicker(time.Duration(x) * time.Second)
   defer ticker.Stop()
@@ -56,7 +56,7 @@ func Worker(mapf func(string, string) []KeyValue,
 }
 
 func MapTask(mapf func(string, string) []KeyValue, filename string, content string, 
-             nReduce int) {
+  nReduce int) {
   if content == ""{
     fmt.Println("content is nil")
     return
@@ -77,10 +77,13 @@ func MapTask(mapf func(string, string) []KeyValue, filename string, content stri
     enc := json.NewEncoder(file)
     enc.Encode(&kv)
   }
+
+  // send RPC done.
+  call("Coordinator.DoneTask", filename, &TaskReply{})
 }
 
 func ReduceTask(reducef func(string, []string) string, filename string, content string, 
-             nReduce int) {
+  nReduce int) {
   if content == ""{
     fmt.Println("content is nil")
     return
@@ -100,13 +103,13 @@ func ReduceTask(reducef func(string, []string) string, filename string, content 
   // read into kv slice from the input file.
   oname := fmt.Sprintf("mr-out-%d", num)
   fmt.Println(oname)
-	ofile, _ := os.Create(oname)
+  ofile, _ := os.Create(oname)
   ifile, err := os.Open(filename)
   if (err != nil) {
     fmt.Println("cannot read input file")
     return
   }
-	kva := []KeyValue{}
+  kva := []KeyValue{}
   dec := json.NewDecoder(ifile)
   for {
     var kv KeyValue
@@ -117,34 +120,36 @@ func ReduceTask(reducef func(string, []string) string, filename string, content 
   }
 
 
-	sort.Sort(ByKey(kva))
+  sort.Sort(ByKey(kva))
 
-	// call Reduce on each distinct key in kva[],
-	// and print the result to ofile.
-	i := 0
-	for i < len(kva) {
-		j := i + 1
-		for j < len(kva) && kva[j].Key == kva[i].Key {
-			j++
-		}
-		values := []string{}
-		for k := i; k < j; k++ {
-			values = append(values, kva[k].Value)
-		}
-		output := reducef(kva[i].Key, values)
+  // call Reduce on each distinct key in kva[],
+  // and print the result to ofile.
+  i := 0
+  for i < len(kva) {
+    j := i + 1
+    for j < len(kva) && kva[j].Key == kva[i].Key {
+      j++
+    }
+    values := []string{}
+    for k := i; k < j; k++ {
+      values = append(values, kva[k].Value)
+    }
+    output := reducef(kva[i].Key, values)
 
-		// this is the correct format for each line of Reduce output.
-		fmt.Fprintf(ofile, "%v %v\n", kva[i].Key, output)
+    // this is the correct format for each line of Reduce output.
+    fmt.Fprintf(ofile, "%v %v\n", kva[i].Key, output)
 
-		i = j
-	}
+    i = j
+  }
 
-	ofile.Close()
+  // send RPC done.
+  call("Coordinator.DoneTask", filename, &TaskReply{})
+  ofile.Close()
+
 }
 
-
 func CallGet() (string, string, int, string){
-	reply := TaskReply{} 
+  reply := TaskReply{} 
   ok := call("Coordinator.GetTask", new(struct {}), &reply)
   if ok {
     // fmt.Printf("contents are %s", reply)
@@ -162,26 +167,26 @@ func CallGet() (string, string, int, string){
 //
 func CallExample() {
 
-	// declare an argument structure.
-	args := ExampleArgs{}
+  // declare an argument structure.
+  args := ExampleArgs{}
 
-	// fill in the argument(s).
-	args.X = 99
+  // fill in the argument(s).
+  args.X = 99
 
-	// declare a reply structure.
-	reply := ExampleReply{}
+  // declare a reply structure.
+  reply := ExampleReply{}
 
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
+  // send the RPC request, wait for the reply.
+  // the "Coordinator.Example" tells the
+  // receiving server that we'd like to call
+  // the Example() method of struct Coordinator.
+  ok := call("Coordinator.Example", &args, &reply)
+  if ok {
+    // reply.Y should be 100.
+    fmt.Printf("reply.Y %v\n", reply.Y)
+  } else {
+    fmt.Printf("call failed!\n")
+  }
 }
 
 //
@@ -190,19 +195,19 @@ func CallExample() {
 // returns false if something goes wrong.
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := coordinatorSock()
-	c, err := rpc.DialHTTP("unix", sockname)
-	if err != nil {
-		log.Fatal("dialing:", err)
-	}
-	defer c.Close()
+  // c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+  sockname := coordinatorSock()
+  c, err := rpc.DialHTTP("unix", sockname)
+  if err != nil {
+    log.Fatal("dialing:", err)
+  }
+  defer c.Close()
 
-	err = c.Call(rpcname, args, reply)
-	if err == nil {
-		return true
-	}
+  err = c.Call(rpcname, args, reply)
+  if err == nil {
+    return true
+  }
 
-	fmt.Println(err)
-	return false
+  fmt.Println(err)
+  return false
 }
